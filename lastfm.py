4bb1d42a14b89cb62975f4602152a14750a29055
Lucas Guo <lucas.lu.guo@gmail.com>
2026-08-28 16:16:06 -0700

    Finish get_artist_tags function to return the TOP_N_TAGS from lastapi gettoptags call

--- lastfm.py
+++ lastfm.py
@@ -15,25 +15,27 @@ REQUEST_DELAY = 0.25
 TOP_N_TAGS = 5
 
 # return up to top_n Last.fm tags for some artist
-# def get_artist_tags(artist: str, top_n: int = TOP_N_TAGS) -> list:
-artist = "Zedd"
-url = "https://ws.audioscrobbler.com/2.0/"
-params = {
-    "method": "artist.gettoptags",
-    "artist": artist,
-    "api_key": LASTFM_API_KEY,
-    "format": "json",
-    "autocorrect": 1
-}
+def get_artist_tags(artist: str) -> list:
+    url = "https://ws.audioscrobbler.com/2.0/"
+    params = {
+        "method": "artist.gettoptags",
+        "artist": artist,
+        "api_key": LASTFM_API_KEY,
+        "format": "json",
+        "autocorrect": 1
+    }
 
-try:
-    response = requests.get(url, params=params, timeout=10)
-    data = response.json()
-except Exception:
-    print(f"! Failed for {artist}: {Exception}")
+    try:
+        response = requests.get(url, params=params, timeout=10)
+        data = response.json()
+        tags = []
+        for tag in data.get("toptags", {}).get("tag", {}):
+            tags.append(tag.get("name"))
+    except Exception:
+        print(f"! Failed for {artist}: {Exception}")
 
-print(data)
+    time.sleep(REQUEST_DELAY)
+    return tags[:TOP_N_TAGS]
 
-time.sleep(REQUEST_DELAY)
-# return tags
+print(get_artist_tags("zedd"))
 
